c2f95686398774a714af0705986eb8c88a32a436
daffaviraka <daffanew99@gmail.com>
2020-03-25 02:57:13 +0700

    Initial commit

---
+++ app/src/main/java/com/example/projectuts/BMTHActivity.java
@@ -0,0 +1,4 @@
+package com.example.projectuts;
+
+class BMTHActivity {
+}
---
+++ app/src/main/java/com/example/projectuts/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.projectuts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void handleBMTH(View view) {
+        Intent intent = new Intent(this, BMTHActivity.class);
+        startActivity(intent);
+    }
+
+    public void handleSlipknot(View view) {
+        Intent intent = new Intent(this, SlipknotActivity.class);
+        startActivity(intent);
+    }
+
+    public void handleNeckDeep(View view) {
+        Intent intent = new Intent(this, NeckDeepActivity.class);
+        startActivity(intent);
+    }
+
+    public void handleBurgerkill(View view) {
+        Intent intent = new Intent(this, BurgerkillActivity.class);
+        startActivity(intent);
+    }
+}
